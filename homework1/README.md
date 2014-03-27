da87c4fe2720a44a1239396a0c2ca2cf5bdbbc80
Conor O'Sullivan <cosullivan66@gmail.com>
2014-03-26 21:28:39 -0700

    Add code and images

--- homework1/README.md
+++ homework1/README.md
@@ -7,8 +7,49 @@ Homework 1: Basic Charts
 
 ## Instructions ##
 
-[INSTRUCTIONS: Include instructions on how to run your code. See [`homework0`](../homework0) for an example.]
+The following packages must be installed prior to running this code:
+
+- `ggplot2`
+- `reshape`
+- `plyr`
+- `scales`
+
+To run this code, please enter the following commands in R:
+
+```
+library(devtools)
+source_url("https://raw.github.com/sjengle/msan622/template/homework0/anscombe.r")
+```
+
+This will generate 4 images.
 
 ## Discussion ##
 
-[DISCUSSION: Include a discussion here if required by the assignment.]
+#Plot 1:
+For this plot I tried to make the title quite descriptive, as it is hard to tell exactly how many points the user is looking at. I found a grey background to be a little easier on the eyes than white, which is what I usually use. I liked including the commas in the budgets, so 100,000,000 instead of 1e8.0. I also included every movie rating because the default was 0, 2.5, 5, 7.5, and 10 which was less helpful to understanding in this context. My thinking in choosing the colors was to differentiate the colors between Drama, Mixed, None, and Comedy as much as possible. They show up the most and the default colors that were assigned to these genres were so similar it was hard to tell them apart at first.
+
+#Plot 2:
+I decided to order the bars from greatest to least instead of their first order, which was alphabetically by genre name. I then made them all the same color because I thought keeping the genre colors the same as the previous plot added no crucial information and was just distracting. I also removed the legend as well as the minor gridlines because neither of those were making the display more informative.
+
+#Plot 3:
+I decided to turn rescale the movie budgets by a factor of one million to improve the readability of the tick mark labels. Also, I removed the legend because the information it provides in this plot is redundant and unnecessary. I preferred the 3-by-3 symmetry of the rows and columns compared to the two rows of 5 graphs and 4 graphs. I would only have chosen 5 and 4 if I was keeping in the legend.
+
+#Plot 4:
+To graph this, I had to melt the data.frame and keep time as an ID variable. The reshape package is not suited for ts objects, so I had to unclass the time variable, melt on the unclassed variable, then reclass it as a time variable for the new data.frame. Once graphed, I kept the minor grid axes in this time as it shows the yearly trends that exist in the data. I relabeled the axes and the legend. I also added commas to the price.
+
+First, notice that many of the simple statistics for these four series are the same:
+
+```R
+> print(stats)
+  group mean_x   mean_y var_x    var_y correlation lm_intercept lm_x_effect
+1     1      9 7.500909    11 4.127269   0.8164205     3.000091   0.5000909
+2     2      9 7.500909    11 4.127629   0.8162365     3.000909   0.5000000
+3     3      9 7.500000    11 4.122620   0.8162867     3.002455   0.4997273
+4     4      9 7.500909    11 4.123249   0.8165214     3.001727   0.4999091
+```
+
+However, when we plot these four series as scatterplots with trend lines, we can see they each have a distinct pattern:
+
+![IMAGE](anscombe_quartet.png)
+
+While this is a simple (and contrived) example, it does help motivate how visualization can help us quickly see patterns in data.
---
+++ homework1/homework1.R
@@ -0,0 +1,98 @@
+library(ggplot2) 
+library(scales)
+data(movies) 
+data(EuStockMarkets)
+setwd("/Users/conorosullivan/Documents/FourthModule/visualization/1_hw")
+genre <- rep(NA, nrow(movies))
+count <- rowSums(movies[, 18:24])
+genre[which(count > 1)] = "Mixed"
+genre[which(count < 1)] = "None"
+genre[which(count == 1 & movies$Action == 1)] = "Action"
+genre[which(count == 1 & movies$Animation == 1)] = "Animation"
+genre[which(count == 1 & movies$Comedy == 1)] = "Comedy"
+genre[which(count == 1 & movies$Drama == 1)] = "Drama"
+genre[which(count == 1 & movies$Documentary == 1)] = "Documentary"
+genre[which(count == 1 & movies$Romance == 1)] = "Romance"
+genre[which(count == 1 & movies$Short == 1)] = "Short"
+movies$genre<-genre
+m<-movies
+m<-subset(movies,budget>0)
+
+eu <- transform(data.frame(EuStockMarkets), time = time(EuStockMarkets))
+
+## Plot 1: Scatterplot
+
+t<-ggplot(m,aes(x=budget, y=rating,color=genre))+
+    geom_point()+
+    ggtitle("Scatterplot of  Movie Rating vs. Movie Budget for 5,000 Movies from IMDb")+
+    xlab("Movie Budget in US Dollars")+
+    ylab("Movie Rating")+
+    theme_grey()+
+    theme(text = element_text(size=20))+
+    scale_x_continuous(labels=comma)+
+    scale_y_continuous(breaks=c(0:10))+
+    scale_color_manual(values=c("red","#8EAE69","green",'orange','brown','blue','turquoise','yellow','black'),name="Genre")+
+    theme(legend.background = element_rect(fill="gray90", size=.5, linetype="dotted"))+
+    theme(panel.grid.minor = element_line(colour = "gray90"))
+#show(t)
+ggsave("hw1-scatter.png",height=8,width=13)
+
+## Plot 2: Bar Chart
+
+m$genre<-as.factor(m$genre)
+n <- within(m, genre<-factor(genre, levels=names(sort(table(genre), decreasing=TRUE))))
+
+p<-ggplot(n,aes(genre))+
+    geom_bar(aes(fill=genre))+
+    xlab("Genre")+
+    ylab("Count")+
+    scale_fill_manual(values=c("lightblue3","lightblue3","lightblue3","lightblue3","lightblue3","lightblue3","lightblue3","lightblue3","lightblue3"))+
+    theme(legend.position="none")+
+    ggtitle("Bar Chart of Movie Counts by Genre")+
+    theme(legend.background = element_rect(fill="gray90", size=.5, linetype="dotted"))+
+    theme(panel.grid.minor = element_line(colour = "gray90"))
+#show(p)
+ggsave("hw1-bar.png",height=8,width=11)
+
+## Plot 3: Small Multiples
+e<-m
+q<-ggplot(e,aes(x=budget/1000000, y=rating,color=genre))+
+  geom_point()+
+  ggtitle("Small Multiples of  Movie Rating vs. Movie Budget for 5,000 Movies from IMDb")+
+  xlab("Movie Budget in Millions of US Dollars")+
+  ylab("Movie Rating")+
+  theme_grey()+
+  theme(text = element_text(size=20))+
+  scale_y_continuous(breaks=c(0:10))+
+  scale_color_manual(values=c("red","#8EAE69","green",'orange','brown','blue','turquoise','yellow','black'),name="Genre")+
+  theme(legend.background = element_rect(fill="gray90", size=.5, linetype="dotted"))+
+  theme(panel.grid.minor = element_line(colour = "gray90"))+
+  theme(legend.position="none")+
+  facet_wrap( ~ genre, ncol=3)
+ggsave("hw1-multiples.png",height=8,width=13)
+
+## Plot 4: Multi-Line Chart
+
+library(reshape)
+library(plyr)
+
+eu1<-eu
+eu1$time<-unclass(eu1$time)
+eu1m<-melt(eu1,id="time")
+eu1m$time<-ts(eu1m$time)
+
+
+y<-ggplot(eu1m,aes(x=time, y=value,color=variable))+
+  geom_line()+
+  scale_y_continuous(labels=comma)+
+  scale_color_discrete(name="Index")+
+  xlab("Time")+
+  ylab("Price in Euro")+
+  ggtitle('Multi-Line Chart of Stock Price vs Time')#+
+  
+ggsave("hw1-multiline.png",height=8,width=11)
+
+
+
+
+
